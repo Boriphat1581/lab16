37efec5a27dc4820a6ce57ace2a97c23e7366ae1
Boriphat1581 <boriphat_palee@cmu.ac.th>
2026-02-06 13:38:47 +0700

    lab16_1

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -4,14 +4,41 @@
 #include<iomanip>
 using namespace std;
 
-void showData(double *,int,int);
+void showData(double *d, int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N * M; i++){
+        cout << *(d + i);
+        if((i + 1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
 
-void randData(double *,int,int);
+void randData(double *d, int N, int M){
+    for(int i = 0; i < N * M; i++){
+        // Generates random number between 0.00 and 1.00
+        *(d + i) = (rand() % 101) / 100.0;
+    }
+}
 
-void findRowSum(const double *,double *,int,int);
+void findRowSum(const double *d, double *sum1, int N, int M){
+    for(int i = 0; i < N; i++){
+        sum1[i] = 0;
+        for(int j = 0; j < M; j++){
+            // Accessing d[i][j] using pointer arithmetic: *(d + (i * M) + j)
+            sum1[i] += *(d + (i * M) + j);
+        }
+    }
+}
 
-void findColSum(const double *,double *,int,int);
-
+void findColSum(const double *d, double *sum2, int N, int M){
+    for(int j = 0; j < M; j++){
+        sum2[j] = 0;
+        for(int i = 0; i < N; i++){
+            // Accessing d[i][j] using pointer arithmetic
+            sum2[j] += *(d + (i * M) + j);
+        }
+    }
+}
 int main(){
 	srand(time(0));
 	const int N = 6, M = 8;
